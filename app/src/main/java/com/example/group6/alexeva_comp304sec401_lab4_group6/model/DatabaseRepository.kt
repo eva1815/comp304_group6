39b5d3ada428e269a1e20a557b2769255b6cd490
PacificZenA <alextoronto66@gmail.com>
2023-03-28 11:56:50 -0400

    Delete unnecessary db and relative code

--- app/src/main/java/com/example/group6/alexeva_comp304sec401_lab4_group6/model/DatabaseRepository.kt
+++ app/src/main/java/com/example/group6/alexeva_comp304sec401_lab4_group6/model/DatabaseRepository.kt
@@ -2,16 +2,14 @@ package com.example.group6.alexeva_comp304sec401_lab4_group6.model
 
 import androidx.lifecycle.LiveData
 import com.example.group6.alexeva_comp304sec401_lab4_group6.entity.Books
-import com.example.group6.alexeva_comp304sec401_lab4_group6.entity.Librarian
 import com.example.group6.alexeva_comp304sec401_lab4_group6.entity.Student
 import com.example.group6.alexeva_comp304sec401_lab4_group6.model.dao.BooksDao
 import com.example.group6.alexeva_comp304sec401_lab4_group6.model.dao.LibrarianDao
 import com.example.group6.alexeva_comp304sec401_lab4_group6.model.dao.StudentDao
-import kotlinx.coroutines.flow.Flow
 
 // Declares the DAO as a private property in the constructor. Pass in the DAO
 // instead of the whole database, because you only need access to the DAO
-class LibrarianRepository(private val librarianDao: LibrarianDao, private val studentDao: StudentDao, private val booksDao: BooksDao) {
+class DatabaseRepository(private val librarianDao: LibrarianDao, private val studentDao: StudentDao, private val booksDao: BooksDao) {
 
     // Room executes all queries on a separate thread.
     // Observed Flow will notify the observer when the data has changed.
--- app/src/main/java/com/example/group6/alexeva_comp304sec401_lab4_group6/viewModel/BooksViewModel.kt
+++ app/src/main/java/com/example/group6/alexeva_comp304sec401_lab4_group6/viewModel/BooksViewModel.kt
@@ -2,9 +2,9 @@ package com.example.group6.alexeva_comp304sec401_lab4_group6.viewModel
 
 
 import androidx.lifecycle.ViewModel
-import com.example.group6.alexeva_comp304sec401_lab4_group6.model.LibrarianRepository
+import com.example.group6.alexeva_comp304sec401_lab4_group6.model.DatabaseRepository
 
-class BooksViewModel(private val repository: LibrarianRepository):ViewModel() {
+class BooksViewModel(private val repository: DatabaseRepository):ViewModel() {
     // Using LiveData and caching what allWords returns has several benefits:
     // - We can put an observer on the data (instead of polling for changes) and only update the
     //   the UI when the data actually changes.
