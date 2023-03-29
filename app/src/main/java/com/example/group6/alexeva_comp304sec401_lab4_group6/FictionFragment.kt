c81bbf7c5a996959e9a52652f0e3211c5ea8f4f7
eva1815 <109616875+eva1815@users.noreply.github.com>
2023-03-28 21:17:37 -0400

    Adding fragments, Adapter, RecycleView

--- app/src/main/java/com/example/group6/alexeva_comp304sec401_lab4_group6/FictionFragment.kt
+++ app/src/main/java/com/example/group6/alexeva_comp304sec401_lab4_group6/FictionFragment.kt
@@ -17,4 +17,5 @@ class FictionFragment : Fragment() {
     }
 
 
+
 }
--- app/src/main/java/com/example/group6/alexeva_comp304sec401_lab4_group6/LibrarianLoginActivity.kt
+++ app/src/main/java/com/example/group6/alexeva_comp304sec401_lab4_group6/LibrarianLoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.group6.alexeva_comp304sec401_lab4_group6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class LibrarianLoginActivity : AppCompatActivity() {
+    private lateinit var librarianLoginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_librarian_login)
+
+        librarianLoginButton = findViewById(R.id.btn_librarian_login)
+        Log.i("TAG", "Librarian Login button was Clicked!")
+        val intent = Intent(this,BooksModuleActivity::class.java)
+        startActivity(intent)
     }
 }
